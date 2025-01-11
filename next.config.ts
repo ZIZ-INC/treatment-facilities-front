e75035c55351306a359696ef35ee5645362ed773
sayat <sayatraykulov@gmail.com>
2025-01-12 00:00:35 +0500

    refactor: update path to i18 config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin(
-    './src/i18n/request.ts'
+    './src/locales/config/request.ts'
 );
 
 const nextConfig: NextConfig = {
