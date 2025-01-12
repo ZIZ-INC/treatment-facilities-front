1f4a8f3d36ca419981689f9e27d81fef21a54acf
sayat <sayatraykulov@gmail.com>
2025-01-12 06:39:45 +0500

    fix: close hamburger menu on w>768

--- src/app/(ui)/_components/Header.tsx
+++ src/app/(ui)/_components/Header.tsx
@@ -1,32 +1,51 @@
-"use client"
-import React, {useState} from "react";
+"use client";
+
+import React, {useState, useEffect} from "react";
 import {signOut, useSession} from "next-auth/react";
 import {LanguageSelect} from "@/shared/components/LanguageSelect";
 import {useTranslations} from "next-intl";
-import {Menu, X, LogOut} from "lucide-react";
+import {Menu, X, LogOut, Moon, Sun} from "lucide-react";
+import {useTheme} from "@/shared/hooks/useTheme";
 
 export function Header() {
     const {data} = useSession();
     const t = useTranslations("app.(ui)._components.Header");
 
     const [menuOpen, setMenuOpen] = useState(false);
+    const {theme, toggleTheme} = useTheme()
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 768) {
+                setMenuOpen(false); // Automatically close menu on desktop dimensions
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
-        <header className="flex justify-between items-center p-4 border-b bg-white relative">
+        <header className="flex justify-between items-center p-4 border-b bg-primaryBackgroundColor relative">
             {/* Title */}
             <h1 className="text-lg md:text-xl font-bold text-primaryColor">
                 {t("title")}
             </h1>
 
-            {/* Mobile: Language Selector and Hamburger Menu */}
+            {/* Mobile: Language Selector, Theme Switcher, and Hamburger Menu */}
             <div className="flex items-center gap-4 md:hidden">
-                <LanguageSelect
-                    wrapperClassName="flex items-center"
-                    className="w-auto"
-                />
+                <LanguageSelect wrapperClassName="flex items-center" className="w-auto"/>
+                <button onClick={toggleTheme} aria-label={t("toggleTheme")}>
+                    {theme === "light" ? (
+                        <Moon className="w-6 h-6 text-gray-600"/>
+                    ) : (
+                        <Sun className="w-6 h-6 text-gray-300"/>
+                    )}
+                </button>
                 <button
                     onClick={() => setMenuOpen((prev) => !prev)}
-                    className="text-gray-600"
+                    className="text-primaryColor"
                     aria-label={menuOpen ? t("closeMenu") : t("openMenu")}
                 >
                     {menuOpen ? <X className="w-6 h-6"/> : <Menu className="w-6 h-6"/>}
@@ -36,10 +55,18 @@ export function Header() {
             {/* Desktop Navigation */}
             <div className="hidden md:flex items-center gap-4 text-primaryColor">
                 <LanguageSelect/>
-                <span className="text-gray-600">{data?.user?.email || t("unknownUser")}</span>
+                <button onClick={toggleTheme} aria-label={t("toggleTheme")}
+                        className="text-primaryColor">
+                    {theme === "light" ? (
+                        <Moon className="w-6 h-6"/>
+                    ) : (
+                        <Sun className="w-6 h-6"/>
+                    )}
+                </button>
+                <span className="text-primaryColor">{data?.user?.email || t("unknownUser")}</span>
                 <button
                     onClick={() => signOut()}
-                    className="flex items-center text-gray-600 hover:text-red-600"
+                    className="flex items-center text-primaryColor hover:text-red-600"
                     aria-label={t("signOut")}
                 >
                     <LogOut className="w-5 h-5 md:w-6 md:h-6 mr-1"/>
@@ -50,11 +77,11 @@ export function Header() {
             {/* Mobile Menu */}
             {menuOpen && (
                 <div
-                    className="absolute top-16 right-4 w-64 bg-primaryBackgroundColor border border-gray-300 rounded-lg shadow-lg p-4 flex flex-col gap-4"
+                    className="absolute top-16 right-4 w-64 bg-primaryBackgroundColor border border-secondaryBorderColor rounded-lg shadow-lg p-4 flex flex-col gap-4"
                 >
                     <div className="flex flex-col gap-2">
                         {/* User Info */}
-                        <div className="text-lg text-gray-600 text-center">
+                        <div className="text-lg text-primaryColor text-center">
                             {data?.user?.email || t("unknownUser")}
                         </div>
 
@@ -62,7 +89,7 @@ export function Header() {
                         <div className="flex justify-end">
                             <button
                                 onClick={() => signOut()}
-                                className="flex w-32 items-center p-2 rounded-lg border border-red-500 justify-center text-red-500"
+                                className="flex w-32 items-center p-2 rounded-lg border border-red-500 justify-center text-errorColor"
                                 aria-label={t("signOut")}
                             >
                                 <LogOut className="w-6 h-6 mr-2"/>
