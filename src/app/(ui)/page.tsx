1d412dbb887d97d95d2402dcae87f4a57f33d9b1
sayat <sayatraykulov@gmail.com>
2025-01-12 06:30:01 +0500

    refactor: remove unnecessary class

--- src/app/(ui)/page.tsx
+++ src/app/(ui)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     const t = useTranslations("app.(ui).page");
     return (
         <div
-            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20"
         > {t("welcomeMessage", {username: data?.user.username})}
         </div>
     );
