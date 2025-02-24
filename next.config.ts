56197ff8e3321bbe43963fd3ff5834100530ce36
sayat <sayatraykulov@gmail.com>
2025-02-24 09:39:14 +0500

    feat: add docker

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,12 @@ const withNextIntl = createNextIntlPlugin(
 );
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    output: "standalone",
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default withNextIntl(nextConfig);
