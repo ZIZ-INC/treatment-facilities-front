b3f4b3beaadd46b23a759a368bfce657c65c3495
sayat <sayatraykulov@gmail.com>
2025-02-24 10:51:07 +0500

    feat: add authentication feature libs

--- src/features/authentication/lib/zod.ts
+++ src/features/authentication/lib/zod.ts
@@ -1,21 +1,32 @@
 import {z} from "zod";
-import {getTranslator} from "@/locales/config/translation";
+import {TranslationValues} from "use-intl";
 
-export const createSignUpSchema = async () => {
-    const t = await getTranslator();
-
+export const createRegisterSchema = (
+    t: (key: string, values?: TranslationValues) => string
+) => {
     return z
         .object({
-            username: z
+            firstname: z
                 .string()
-                .min(3, t("auth.errors.username.min"))
-                .max(30, t("auth.errors.username.max")),
+                .min(1, t("auth.errors.firstname.min")),
+            lastname: z
+                .string()
+                .min(1, t("auth.errors.lastname.min")),
             email: z.string().email(t("auth.errors.email.invalid")),
+            age: z
+                .coerce.number({
+                    invalid_type_error: t("auth.errors.age.typeError"),
+                })
+                .min(0, t("auth.errors.age.min"))
+                .max(70, t("auth.errors.age.max")),
+            status: z
+                .string()
+                .min(1, t("auth.errors.status.required")),
             password: z
                 .string()
-                .min(8, t("auth.errors.password.min"))
+                .min(8, t("auth.errors.password.min", { min: 8 }))
                 .regex(
-                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
                     t("auth.errors.password.weak")
                 ),
             confirm: z.string(),
@@ -26,11 +37,19 @@ export const createSignUpSchema = async () => {
         });
 };
 
-export const createSignInSchema = async () => {
-    const t = await getTranslator();
-
+export const createLoginSchema = (
+    t: (key: string, values?: TranslationValues) => string
+) => {
     return z.object({
         email: z.string().email(t("auth.errors.email.invalid")),
-        password: z.string().min(8, t("auth.errors.password.min")),
+        password: z.string().min(5, t("auth.errors.password.min", {min: 5})),
+    });
+};
+
+export const createResetSchema = (
+    t: (key: string, values?: TranslationValues) => string
+) => {
+    return z.object({
+        email: z.string().email(t("auth.errors.email.invalid")),
     });
 };
